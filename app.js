d85e51826d9a2eedaec7b274b464dbeb53fa0ed1
Ashis Pavan <ashispavan@live.com>
2017-02-13 15:19:40 +0530

    Integrated Foursquare API

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 var map;
 var markers = [];
 var locations = [
-    { title: 'Park Ave Penthouse', location: { lat: 40.7713024, lng: -73.9632393 } },
-    { title: 'Chelsea Loft', location: { lat: 40.7444883, lng: -73.9949465 } },
-    { title: 'Union Square Open Floor Plan', location: { lat: 40.7347062, lng: -73.9895759 } },
-    { title: 'East Village Hip Studio', location: { lat: 40.7281777, lng: -73.984377 } },
-    { title: 'TriBeCa Artsy Bachelor Pad', location: { lat: 40.7195264, lng: -74.0089934 } },
-    { title: 'Chinatown Homey Space', location: { lat: 40.7180628, lng: -73.9961237 } }
+    { title: 'Burger Joint', location: { lat: 40.7643, lng: -73.9786 } },
+    { title: 'Bill\'s Bar and Burger', location: { lat: 40.7593, lng: -73.9775 } },
+    { title: 'JG Melon', location: { lat: 40.7711, lng: -73.9593 } },
+    { title: 'B and B Winepub', location: { lat: 40.7257, lng: -73.9980 } },
+    { title: 'The Spotted Pig', location: { lat: 40.7356, lng: -74.0067 } },
+    { title: 'New York Burger Co', location: { lat: 40.7416, lng: -73.9933 } }
 ];
 
 var listItems = locations.map(function(item) {
@@ -44,23 +44,40 @@ function initMap() {
     ko.applyBindings(new ViewModel(largeInfowindow));
 }
 
+
 function populateInfoWindow(marker, infowindow) {
+    var client_id = 'AQSGIQYJFQIUZUAIBNGBWMN0UELAZHYN4HBNTETXQFC1QROG';
+    var client_secret = 'MZUV4WHGSEKL25Z353K0PIRBFAKQEUABDSLLYSEQEWNEZ4FG', content = '', address = '';
+    var URL = "https://api.foursquare.com/v2/venues/search?v=20161016&ll=" + marker.position.lat() + ", " + marker.position.lng()
+    + "&query=" + marker.title + "&intent=checkin&client_id=" + client_id + "&client_secret=" + client_secret;
+    
     if (infowindow.marker != marker) {
-        infowindow.setContent(marker.title);
-        infowindow.marker = marker;
-        infowindow.addListener('closeclick', function() {
-            infowindow.marker = null;
-            marker.setAnimation(null);
-        });
-        infowindow.open(map, marker);
-    }
-    for (var i in markers) {
-        markers[i].setAnimation(null);
-        if (markers[i].id == marker.id) {
-            markers[i].setAnimation(google.maps.Animation.BOUNCE);
-        }
+         $.ajax({
+            url: URL,
+            dataType: "json",
+            async: true,
+            success: function(data){
+                content = data.response.venues[0].name;
+                address = data.response.venues[0].location.formattedAddress[0];
+                infowindow.setContent('<div>' + '<h5>' + content + '</h5>' + ' <p>' + address + '</p>' + '</div>');
+                infowindow.marker = marker;
+                infowindow.addListener('closeclick', function() {
+                    infowindow.marker = null;
+                    marker.setAnimation(null);
+                 });
+                infowindow.open(map, marker);
+                for (var i in markers) {
+                    markers[i].setAnimation(null);
+                    if (markers[i].id == marker.id) {
+                        markers[i].setAnimation(google.maps.Animation.BOUNCE);
+                    }
+                }
+            }   
+        });     
     }
 }
+
+
 
 var ListItemModel = function() {
     this.listItems = ko.observable([]);
